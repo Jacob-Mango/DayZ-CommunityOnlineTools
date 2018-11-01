357208f8e05cebe407a98eab8aaa88b90b88f694
Jacob-Mango <mangosprograms@gmail.com>
2018-11-01 21:48:39 +1100

    - Added ObjectModule

--- COS/Modules/Position/module.c
+++ COS/Modules/Position/module.c
@@ -1,28 +1,6 @@
+#include "COS\\Modules\\Position\\PositionModule.c"
+
 #include "COS\\Modules\\Position\\gui\\PositionMenu.c"
-
-class PositionModule: EditorModule
-{
-    void PositionModule()
-    {
-        GetRPCManager().AddRPC( "COS_Teleport", "SetPosition", this, SingeplayerExecutionType.Server );
-    }
-
-    string GetLayoutRoot()
-    {
-        return "COS\\Modules\\Position\\gui\\layouts\\PositionMenu.layout";
-    }
-    
-    void SetPosition( CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target )
-    {
-        Param1< vector > data;
-        if ( !ctx.Read( data ) ) return;
-
-        if( type == CallType.Server )
-        {	
-            target.SetPosition( data.param1 );
-        }
-    }
-}
 
 void RegisterModules( ModuleManager moduleManager )
 {
