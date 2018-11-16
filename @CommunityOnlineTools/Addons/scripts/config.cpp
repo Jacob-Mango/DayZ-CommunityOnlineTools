ad495c32a19fc42a775681e3e7e4f9e0ef56e796
Jacob-Mango <mangosprograms@gmail.com>
2018-11-16 14:15:16 +1100

    - Removed the mod.cpp

--- @CommunityOnlineTools/Addons/scripts/config.cpp
+++ @CommunityOnlineTools/Addons/scripts/config.cpp
@@ -11,3 +11,45 @@ class CfgPatches
         };
     };
 };
+
+class CfgMods
+{
+	class CommunityOnlineTools
+	{
+		dir = "COT";
+		picture = "";
+		action = "";
+		hideName = 1;
+		hidePicture = 1;
+		name = "Community Online Tools";
+		credits = "Jacob_Mango, DannyDog, Arkensor";
+		author = "Jacob_Mango, DannyDog, Arkensor";
+		authorID = "0"; 
+		version = "1.0"; 
+		extra = 0;
+		type = "mod";
+		
+		dependencies[] = {"Game", "World", "Mission"};
+		
+		class defs
+		{
+			class gameScriptModule
+			{
+				value = "";
+				files[] = {"COT/Scripts/3_Game"};
+			};
+
+			class worldScriptModule
+			{
+				value = "";
+				files[] = {"COT/Scripts/4_World"};
+			};
+
+			class missionScriptModule
+			{
+				value = "";
+				files[] = {"COT/Scripts/5_Mission","COT/Modules/Camera","COT/Modules/Game","COT/Modules/Object","COT/Modules/Teleport","COT/Modules/Weather"};
+			};
+		}
+	};
+};
