20bd85e24cd35851150838da36cb114ddf68d795
Jacob-Mango <mangosprograms@gmail.com>
2018-11-20 00:01:41 +1100

    - Added more UI template

--- @CommunityOnlineTools/Addons/scripts/5_Mission/gui/MapEditor/MapEditorMenu.c
+++ @CommunityOnlineTools/Addons/scripts/5_Mission/gui/MapEditor/MapEditorMenu.c
@@ -21,6 +21,20 @@ class MapEditorMenu
         layoutRoot = GetGame().GetWorkspace().CreateWidgets( "COT\\gui\\layouts\\Map\\MapMenu.layout" );
         layoutRoot.Show( false );
 
+        ref Widget objectInfoWrapper = layoutRoot.FindAnyWidget( "object_info_wrapper" );
+
+        ref Widget objectInfoGrid = UIActionManager.CreateGridSpacer( objectInfoWrapper, 1, 2 );
+
+        UIActionManager.CreateEditableVector( objectInfoGrid, "Position: " );
+        UIActionManager.CreateEditableVector( objectInfoGrid, "Rotation: " );
+
+
+        ref Widget objectControlsWrapper = layoutRoot.FindAnyWidget( "object_controls" );
+
+        ref Widget objectControlsGrid = UIActionManager.CreateGridSpacer( objectControlsWrapper, 1, 2 );
+        UIActionManager.CreateText( objectControlsGrid, "Select: ", "Left Mouse" );
+        UIActionManager.CreateText( objectControlsGrid, "Delete: ", "Delete" );
+
         return layoutRoot;
     }
 
