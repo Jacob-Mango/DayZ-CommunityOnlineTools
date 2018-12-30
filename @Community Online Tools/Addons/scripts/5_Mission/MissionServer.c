23f60cbc3da447748598bfdb434022f6fef45e50
Jacob-Mango <mangosprograms@gmail.com>
2018-12-31 02:31:31 +1100

    - Fixed default permissions and a file crash.

--- @Community Online Tools/Addons/scripts/5_Mission/MissionServer.c
+++ @Community Online Tools/Addons/scripts/5_Mission/MissionServer.c
@@ -1,9 +1,13 @@
 modded class MissionServer
 {
+    protected bool m_bLoaded;
+
     protected ref CommunityOnlineTools m_Tool;
 
     void MissionServer()
     {        
+        m_bLoaded = false;
+
         m_Tool = new CommunityOnlineTools();
     }
 
@@ -21,7 +25,17 @@ modded class MissionServer
         super.OnMissionStart();
 
         m_Tool.OnStart();
+    }
 
+    override void OnMissionFinish()
+    {
+        m_Tool.OnFinish();
+
+        super.OnMissionFinish();
+    }
+
+    void OnLoaded()
+    {
         ref array< string > data = new ref array< string >;
         GetPermissionsManager().RootPermission.Serialize( data );
 
@@ -34,25 +48,27 @@ modded class MissionServer
         {
             for ( int i = 0; i < data.Count(); i++ )
             {
-                data[i].Replace( " 0", " 2" );
+                string s = data[i];
+                s.Replace( "0", "2" );
+                data.Remove( i );
+                data.InsertAt( s, i );
             }
 
             GetPermissionsManager().CreateRole( "admin", data );
         }
     }
 
-    override void OnMissionFinish()
-    {
-        m_Tool.OnFinish();
-
-        super.OnMissionFinish();
-    }
-
     override void OnUpdate( float timeslice )
     {
-        super.OnUpdate( timeslice );
+        if( !m_bLoaded && !GetDayZGame().IsLoading() )
+        {
+            m_bLoaded = true;
+            OnLoaded();
+        } else {
+            super.OnUpdate( timeslice );
 
-        m_Tool.OnUpdate( timeslice );
+            m_Tool.OnUpdate( timeslice );
+        }
     }
 
     override void OnPreloadEvent(PlayerIdentity identity, out bool useDB, out vector pos, out float yaw, out int queueTime)
