8529182d02e46201fee87ad4b132bff0f4d83d4e
Jacob-Mango <mangosprograms@gmail.com>
2018-12-30 19:55:41 +1100

    - Added default roles - Fix script errors

---
+++ @Community Online Tools/Addons/scripts/4_World/permissions/PlayerFile.c
@@ -0,0 +1,13 @@
+modded class PlayerFile
+{
+    override bool Load( PlayerIdentity identity )
+    {
+        bool foundFile = false;
+        foundFile = super.Load( identity );
+
+        if ( !foundFile )
+        {
+            Roles.Insert( "everyone" );
+        }
+    }
+}
