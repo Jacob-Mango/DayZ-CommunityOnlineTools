4f2bcd2e8529bc7ea0293d6f0bd517d047cf2c49
Jacob-Mango <mangosprograms@gmail.com>
2019-05-18 19:21:06 +1000

    Improved Debugging

--- JM/COT/scripts/3_Game/CommunityOnlineTools/Debug/Debugging.c
+++ JM/COT/scripts/3_Game/CommunityOnlineTools/Debug/Debugging.c
@@ -1,8 +1,11 @@
 modded class Debugging
 {	
-    bool JM_COT_StaticFunctions;
-	bool JM_COT_WidgetStore;
-	bool JM_COT_ModuleFramework;
-	bool JM_COT_PermissionFramework;
-	bool JM_COT_Mission;
+    void Debugging()
+	{
+		Modes.Insert( "JM_COT_StaticFunctions", false );
+		Modes.Insert( "JM_COT_WidgetStore", false );
+		Modes.Insert( "JM_COT_ModuleFramework", false );
+		Modes.Insert( "JM_COT_PermissionFramework", false );
+		Modes.Insert( "JM_COT_Mission", false );
+	}
 }
