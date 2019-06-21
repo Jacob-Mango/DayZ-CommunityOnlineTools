03f9cdfed96be905382acd5c56a2b1ae4f04ebee
Jacob-Mango <mangosprograms@gmail.com>
2019-06-21 23:21:50 +1000

    1.04 Fix

--- JM/COT/scripts/5_Mission/CommunityOnlineTools/MissionServer.c
+++ JM/COT/scripts/5_Mission/CommunityOnlineTools/MissionServer.c
@@ -78,13 +78,6 @@ modded class MissionServer
 		}
 	}
 
-	override void OnPreloadEvent(PlayerIdentity identity, out bool useDB, out vector pos, out float yaw, out int queueTime)
-	{
-		super.OnPreloadEvent( identity, useDB, pos, yaw, queueTime );
-
-		// queueTime = 0;
-	}
-
 	override void InvokeOnConnect( PlayerBase player, PlayerIdentity identity)
 	{
 		super.InvokeOnConnect( player, identity );
