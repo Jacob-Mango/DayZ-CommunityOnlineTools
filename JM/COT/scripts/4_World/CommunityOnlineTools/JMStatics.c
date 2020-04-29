fafdf478b5d83e7a10b5b07dc24ae5f892518c28
Jacob-Mango <mangosprograms@gmail.com>
2020-04-30 02:47:02 +1000

    Sorting

--- JM/COT/scripts/4_World/CommunityOnlineTools/JMStatics.c
+++ JM/COT/scripts/4_World/CommunityOnlineTools/JMStatics.c
@@ -19,4 +19,51 @@ class JMStatics
             arr.Insert( strs[i] );
 		}
 	}
+
+    static void SortStringArrayKVPair( out array< string > arrKey, out array< string > arrValue )
+	{
+		if ( arrKey.Count() != arrValue.Count() )
+			return;
+
+        int count = arrKey.Count();
+
+		string strs[ 1000 ];
+		int pIndices[ 1000 ];
+
+		for ( int i = 0; i < count; i++ )
+		{
+			strs[ i ] = arrKey[ i ];
+		}
+
+		Sort( strs, count );
+
+		for ( i = 0; i < arrKey.Count(); i++ )
+		{
+			for ( int j = 0; j < arrKey.Count(); j++ )
+			{
+				if ( strs[j] == arrKey[i] )
+				{
+					pIndices[i] = j;
+				}
+			}
+		}
+
+		array< string > narrKey = new array< string >;
+		array< string > narrValue = new array< string >;
+
+		narrKey.Resize( count );
+		narrValue.Resize( count );
+
+		for ( i = 0; i < count; i++ )
+		{
+			narrKey.Set( pIndices[i], arrKey[i] );
+            narrValue.Set( pIndices[i], arrValue[i] );
+		}
+
+		arrKey.Clear();
+		arrValue.Clear();
+
+		arrKey.Copy( narrKey );
+		arrValue.Copy( narrValue );
+	}
 };
