e1ebc490c07ad397e1a1b6bf60b897665a7e387f
Tyler Paul <tylerpaul2329@gmail.com>
2020-12-11 19:59:17 -0500

    New Feature Smile :)

--- JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Namalsk/JMEventSpawnerForm.c
+++ JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Namalsk/JMEventSpawnerForm.c
@@ -27,20 +27,29 @@ class JMEventSpawnerForm: JMFormBase
 		return Class.CastTo(m_Module, mdl);
 	}
 	
+	
+	private void AddSimpleButton(string text, string button_text, string function)
+	{
+		Widget wrapper = UIActionManager.CreatePanel(m_ActionsWrapper, 0x00000000, 30);
+		UIActionBase name = UIActionManager.CreateText(wrapper, text);
+		name.SetWidth(0.8);
+		
+		UIActionButton button = UIActionManager.CreateButton(wrapper, button_text, this, function);
+		button.SetData(new JMEventSpawnerButtonData(text));
+		button.SetWidth(0.2);
+		button.SetPosition(0.8);
+
+		m_EventButtons.Insert(button);
+	}
+	
 	override void OnShow()
 	{
 		foreach (string event_name: EVENT_TYPES) {
-			Widget wrapper = UIActionManager.CreatePanel(m_ActionsWrapper, 0x00000000, 30);
-			UIActionBase name = UIActionManager.CreateText(wrapper, event_name);
-			name.SetWidth(0.8);
-			
-			UIActionButton button = UIActionManager.CreateButton(wrapper, "Start", this, "BeginEvent");
-			button.SetData(new JMEventSpawnerButtonData(event_name));
-			button.SetWidth(0.2);
-			button.SetPosition(0.8);
-	
-			m_EventButtons.Insert(button);
+			AddSimpleButton(event_name, "Start", "BeginEvent");
 		}
+		
+		AddSimpleButton("Make server shit pants", "Now", "ShitPants");
+		
 		
 		m_sclr_MainActions.UpdateScroller();
 	}
@@ -53,5 +62,11 @@ class JMEventSpawnerForm: JMFormBase
 		ScriptRPC rpc = new ScriptRPC();
 		rpc.Write(data.ClassName);
 		rpc.Send(null, JMEventSpawnerRPC.StartEvent, true, null);
+	}
+	
+	void ShitPants(UIEvent eid, ref UIActionBase action)
+	{
+		ScriptRPC rpc = new ScriptRPC();
+		rpc.Send(null, JMEventSpawnerRPC.ShitPantsServer, true, null);
 	}
 }
