90ffa3ad0429079a3f08f55aeb75ddc53e37a008
Jacob-Mango <mangosprograms@gmail.com>
2021-02-16 15:06:28 +1100

    Permissions and stamina

--- JM/COT/Scripts/4_World/CommunityOnlineTools/Classes/StaminaHandler.c
+++ JM/COT/Scripts/4_World/CommunityOnlineTools/Classes/StaminaHandler.c
@@ -2,8 +2,12 @@ modded class StaminaHandler
 {
     override void Update(float deltaT, int pCurrentCommandID)
 	{
-        if (m_Player && m_Player.COTHasUnlimitedStamina()) return;
-        
+        if (m_Player && m_Player.COTHasUnlimitedStamina())
+        {
+			m_Player.SetStamina(m_Stamina, m_StaminaCap);
+            return;
+        }
+
         super.Update(deltaT, pCurrentCommandID);
     }
 };
