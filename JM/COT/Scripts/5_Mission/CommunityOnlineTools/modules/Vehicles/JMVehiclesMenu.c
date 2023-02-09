98da5d6ac48cac1aae04b858a771fd3b8bd313e9
lava76 <lava76+github@mail.cbw-clan.de>
2023-02-09 22:07:14 +0100

    Moved to Expansion

--- JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Vehicles/JMVehiclesMenu.c
+++ JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Vehicles/JMVehiclesMenu.c
@@ -145,27 +145,7 @@ class JMVehiclesMenu: JMFormBase
 
 	private void GetVehicleTypeInfo( int type, out int color, out string marker )
 	{
-		#ifdef EXPANSIONMODVEHICLE
-		if ( ( type & JMVT_CAR ) != 0 )
-		{
-			color = ARGB( 255, 243, 156, 18 );
-			marker = "DayZExpansion\\Core\\GUI\\icons\\marker\\marker_car.paa";
-		} else if ( ( type & JMVT_BOAT ) != 0 )
-		{
-			color = ARGB( 255, 41, 128, 185 );
-			marker = "DayZExpansion\\Core\\GUI\\icons\\marker\\marker_boat.paa";
-		} else if ( ( type & JMVT_HELICOPTER ) != 0 )
-		{
-			color = ARGB( 255, 142, 68, 173 );
-			marker = "DayZExpansion\\Core\\GUI\\icons\\marker\\marker_helicopter.paa";
-		} else if ( ( type & JMVT_PLANE ) != 0 )
-		{
-			color = ARGB( 255, 34, 166, 179 );
-			marker = "DayZExpansion\\Core\\GUI\\icons\\marker\\marker_helicopter.paa"; //! TODO: Change to a plane marker
-		}
-		#else
 		color = ARGB( 255, 243, 156, 18 );
-		#endif
 	}
 
 	void SetVehicleInfo(JMVehicleMetaData vehicle)
