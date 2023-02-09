4d141273ad4b2e57f6ab72d86ed6ef8ba8df0a94
lava76 <lava76+github@mail.cbw-clan.de>
2023-02-09 23:51:16 +0100

    Prevent bindings still being processed when edit box has focus

---
+++ JM/COT/Scripts/3_Game/CommunityOnlineTools/InputBindings/CF_InputBindings.c
@@ -0,0 +1,13 @@
+#ifndef CF_INPUTBINDINGS_FOCUS_FIX
+modded class CF_InputBindings
+{
+	override void Update( float dt )
+	{
+		auto focus = GetFocus();
+		if ( focus && focus.IsInherited( EditBoxWidget ) && focus.IsVisible() && !GetUApi().GetInputByID( UAUIMenu ).LocalPress() )
+			return;
+
+		super.Update( dt );
+	}
+};
+#endif
