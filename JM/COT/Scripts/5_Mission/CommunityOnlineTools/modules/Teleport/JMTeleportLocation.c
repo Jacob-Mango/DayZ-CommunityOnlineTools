c8a1328951a911c6974164ddd784b90206449d1a
lava76 <lava76+github@mail.cbw-clan.de>
2023-02-11 16:21:20 +0100

    Proper teleport serialization

--- JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Teleport/JMTeleportLocation.c
+++ JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Teleport/JMTeleportLocation.c
@@ -12,4 +12,26 @@ class JMTeleportLocation
 		Position = position;
 		Radius = radius;
 	}
+
+	void Write( ParamsWriteContext ctx )
+	{
+		ctx.Write( Name );
+		ctx.Write( Permission );
+		ctx.Write( Position );
+		ctx.Write( Radius );
+	}
+
+	bool Read( ParamsReadContext ctx )
+	{
+		if ( !ctx.Read( Name ) )
+			return false;
+		if ( !ctx.Read( Permission ) )
+			return false;
+		if ( !ctx.Read( Position ) )
+			return false;
+		if ( !ctx.Read( Radius ) )
+			return false;
+
+		return true;
+	}
 };
