e0bd9470a31a0a252221453904d94abb60deb84d
lava76 <lava76+github@mail.cbw-clan.de>
2023-02-23 13:38:10 +0100

    Disable actions while COT is open

---
+++ JM/COT/Scripts/4_World/CommunityOnlineTools/Classes/UserActionsComponent/ActionBase.c
@@ -0,0 +1,15 @@
+#ifndef SERVER
+modded class ActionBase
+{
+	override bool Can(PlayerBase player, ActionTarget target, ItemBase item, int condition_mask)
+	{
+		if (!super.Can(player, target, item, condition_mask))
+			return false;
+
+		if (GetCommunityOnlineToolsBase().IsOpen() || GetCOTWindowManager().Count() > 0)
+			return false;
+
+		return true;
+	}
+}
+#endif
