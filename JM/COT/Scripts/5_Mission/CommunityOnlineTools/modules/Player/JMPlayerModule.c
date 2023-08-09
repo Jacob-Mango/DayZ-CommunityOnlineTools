0c129c4bcb03232c7358950b6374b18bd0b090da
lava76 <lava76+github@mail.cbw-clan.de>
2023-08-09 14:46:02 +0200

    CF_BUGFIX_REF

--- JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Player/JMPlayerModule.c
+++ JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Player/JMPlayerModule.c
@@ -134,7 +134,11 @@ class JMPlayerModule: JMRenderableModuleBase
 		return JMPlayerModuleRPC.COUNT;
 	}
 
+#ifdef CF_BUGFIX_REF
+	override void OnRPC( PlayerIdentity sender, Object target, int rpc_type, ParamsReadContext ctx )
+#else
 	override void OnRPC( PlayerIdentity sender, Object target, int rpc_type, ref ParamsReadContext ctx )
+#endif
 	{
 		switch ( rpc_type )
 		{
--- JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Vehicles/JMVehiclesModule.c
+++ JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Vehicles/JMVehiclesModule.c
@@ -264,7 +264,11 @@ class JMVehiclesModule: JMRenderableModuleBase
 		return JMVehiclesModuleRPC.COUNT;
 	}
 
+#ifdef CF_BUGFIX_REF
+	override void OnRPC( PlayerIdentity sender, Object target, int rpc_type, ParamsReadContext ctx )
+#else
 	override void OnRPC( PlayerIdentity sender, Object target, int rpc_type, ref ParamsReadContext ctx )
+#endif
 	{
 		switch ( rpc_type )
 		{
