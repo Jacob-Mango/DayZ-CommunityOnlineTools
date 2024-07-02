471ef739ecea6c23b1cdb4c115b164d51dc97f5b
LieutenantMaster <master1animation@gmail.com>
2024-07-03 00:34:35 +0200

    Better TP menu

--- JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Teleport/JMTeleportForm.c
+++ JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Teleport/JMTeleportForm.c
@@ -1,7 +1,7 @@
 class JMTeleportForm: JMFormBase
 {
 	protected Widget m_ActionsFilterWrapper;
-	protected UIActionEditableText m_Filter;
+	protected UIActionEditableTextPreview m_Filter;
 
 	protected string m_CurrentCategory = "ALL";
 	protected ref TStringArray m_Categories;
@@ -15,6 +15,16 @@ class JMTeleportForm: JMFormBase
 	protected UIActionText m_PositionZ;
 	protected UIActionButton m_Teleport;
 
+	protected UIActionText m_InputTextLocation;
+	protected UIActionText m_InputTextCategory;
+
+	protected UIActionEditableText m_InputLocation;
+	protected UIActionEditableTextPreview m_InputCategory;
+
+	protected UIActionButton m_InputAdd;
+	protected UIActionButton m_InputRefresh;
+	protected UIActionButton m_InputRemove;
+
 	private JMTeleportModule m_Module;
 
 	protected override bool SetModule( JMRenderableModuleBase mdl )
@@ -22,14 +32,13 @@ class JMTeleportForm: JMFormBase
 		return Class.CastTo( m_Module, mdl );
 	}
 
-	//! TODO: Allow admins to add new positions from this menu
 	override void OnInit()
 	{
 		m_ActionsFilterWrapper = layoutRoot.FindAnyWidget( "actions_filter_wrapper" );
 
 		Widget topRow = UIActionManager.CreateGridSpacer( m_ActionsFilterWrapper, 1, 2 );
 
-		m_Filter = UIActionManager.CreateEditableText( topRow, "#STR_COT_TELEPORT_MODULE_FILTER", this, "Type_UpdateList" );
+		m_Filter = UIActionManager.CreateEditableTextPreview( topRow, "#STR_COT_TELEPORT_MODULE_FILTER", this, "Type_UpdateList" );
 		
 		m_CategoriesList = UIActionManager.CreateSelectionBox( topRow, "", {"ALL"}, this, "Click_LocationType" );
 		m_CategoriesList.SetSelectorWidth(1.0);
@@ -51,6 +60,92 @@ class JMTeleportForm: JMFormBase
 		{
 			m_Teleport = UIActionManager.CreateButton( m_ActionsWrapper, "#STR_COT_TELEPORT_MODULE_TELEPORT_ONLINE", this, "Click_Teleport" );
 		}
+
+		m_ActionsWrapper = layoutRoot.FindAnyWidget( "actions_wrapper" );
+
+		Widget inputRows = UIActionManager.CreateGridSpacer( m_ActionsWrapper, 2, 2 );
+
+		m_InputTextLocation = UIActionManager.CreateText( inputRows, "Name" );
+		m_InputTextCategory = UIActionManager.CreateText( inputRows, "Category" );
+
+		m_InputLocation = UIActionManager.CreateEditableText( inputRows, "", this );
+		m_InputLocation.SetWidgetWidth( m_InputLocation.GetLabelWidget(), 0.0 );
+		m_InputLocation.SetWidgetWidth( m_InputLocation.GetEditBoxWidget(), 1.0 );
+
+		m_InputCategory = UIActionManager.CreateEditableTextPreview( inputRows, "", this, "InputCategory_OnChange" );
+		m_InputCategory.SetWidgetWidth( m_InputCategory.GetLabelWidget(), 0.0 );
+		m_InputCategory.SetWidgetWidth( m_InputCategory.GetEditBoxWidget(), 1.0 );
+		
+		Widget inputBtnRows = UIActionManager.CreateGridSpacer( m_ActionsWrapper, 1, 3 );
+
+		m_InputAdd = UIActionManager.CreateButton( inputBtnRows, "Add", this, "Click_AddLocation" );
+		m_InputRefresh = UIActionManager.CreateButton( inputBtnRows, "Refresh", this, "Click_Refresh" );
+		m_InputRemove = UIActionManager.CreateButton( inputBtnRows, "Delete", this, "Click_RemoveLocation" );
+		m_InputAdd.SetColor(COLOR_GREEN_A);
+		m_InputRemove.SetColor(COLOR_RED_A);
+
+		if ( !GetPermissionsManager().HasPermission( "Admin.Player.Teleport.Location.Add" ) )
+		{
+			m_InputTextLocation.Disable();
+			m_InputTextCategory.Disable();
+			m_InputLocation.Disable();
+			m_InputCategory.Disable();
+			m_InputAdd.Disable();
+		}
+
+		if ( !GetPermissionsManager().HasPermission( "Admin.Player.Teleport.Location.Remove" ) )
+			m_InputRemove.Disable();
+		
+		if ( !GetPermissionsManager().HasPermission( "Admin.Player.Teleport.Location.Refresh" ) )
+			m_InputRefresh.Disable();
+	}
+
+	void Click_Refresh( UIEvent eid, UIActionBase action )
+	{
+		if ( eid != UIEvent.CLICK )
+			return;
+
+		if ( !GetPermissionsManager().HasPermission( "Admin.Player.Teleport.Location.Refresh" ) )
+			return;
+
+		m_Module.Reload();
+	}
+
+	void Click_RemoveLocation( UIEvent eid, UIActionBase action )
+	{
+		if ( eid != UIEvent.CLICK )
+			return;
+
+		if ( !GetPermissionsManager().HasPermission( "Admin.Player.Teleport.Location.Remove" ) )
+			return;
+
+		CreateConfirmation_Two( JMConfirmationType.INFO, "Are you sure?", "Delete "+ GetCurrentPositionName(), "#STR_COT_GENERIC_YES", "RemoveLocation_Confirmed", "#STR_COT_GENERIC_NO", "" );
+	}
+
+	void RemoveLocation_Confirmed()
+	{
+		m_Module.RemoveLocation(GetCurrentLocation());
+
+		if ( !GetPermissionsManager().HasPermission( "Admin.Player.Teleport.Location.Refresh" ) )
+			return;
+
+		m_Module.Reload();
+	}
+
+	void Click_AddLocation( UIEvent eid, UIActionBase action )
+	{
+		if ( eid != UIEvent.CLICK )
+			return;
+
+		if ( !GetPermissionsManager().HasPermission( "Admin.Player.Teleport.Location.Add" ) )
+			return;
+
+		m_Module.AddLocation(m_InputLocation.GetText(),  m_InputCategory.GetText(), GetGame().GetPlayer().GetPosition() );
+
+		if ( !GetPermissionsManager().HasPermission( "Admin.Player.Teleport.Location.Refresh" ) )
+			return;
+
+		m_Module.Reload();
 	}
 
 	override void OnShow()
@@ -68,13 +163,6 @@ class JMTeleportForm: JMFormBase
 		{
 			m_Module.Load();
 		}
-	}
-
-	override void OnHide()
-	{
-		super.OnHide();
-
-		//m_Module.OnSelectLocation(vector.Zero, true);
 	}
 
 	override void OnSettingsUpdated()
@@ -107,6 +195,42 @@ class JMTeleportForm: JMFormBase
 		m_Module.Location( GetCurrentLocation(), players );
 	}
 
+	void InputCategory_OnChange( UIEvent eid, UIActionBase action )
+	{
+		if ( eid != UIEvent.CHANGE )
+			return;
+
+		m_InputCategory.SetTextPreview("");
+		
+		string strSearch = m_InputCategory.GetText();
+		if ( strSearch != "" )
+		{
+			string closestMatch;
+			TStringArray suggestions = new TStringArray;
+			foreach(string category: m_Categories)
+			{
+				if ( category == strSearch )
+				{
+					suggestions.Clear();
+					closestMatch = category;
+				}
+				else if ( category.IndexOf(strSearch) == 0 )
+				{
+					if (!closestMatch)
+						suggestions.Insert(category);
+				}
+			}
+
+			if (suggestions.Count())
+			{
+				suggestions.Sort();
+				closestMatch = suggestions[0];
+			}
+
+			m_InputCategory.SetTextPreview(closestMatch);
+		}
+	}
+
 	void Type_UpdateList( UIEvent eid, UIActionBase action )
 	{
 		if ( eid != UIEvent.CHANGE )
@@ -122,7 +246,7 @@ class JMTeleportForm: JMFormBase
 
 		if ( !GetPermissionsManager().HasPermission( "Admin.Player.Teleport.Location" ) )
 			return;
-		
+
 		if(!m_Categories)
 		{
 			m_Categories = new TStringArray;
@@ -131,30 +255,52 @@ class JMTeleportForm: JMFormBase
 				m_CategoriesList.SetSelections(m_Categories);
 		}
 
+		m_Filter.SetTextPreview("");
 		m_LstPositionList.ClearItems();
 
-		string filter = "" + m_Filter.GetText();
+		string filter = m_Filter.GetText();
 		filter.ToLower();
 
 		array< ref JMTeleportLocation > locations = m_Module.GetLocations();
 		if ( !locations )
 			return;
 		
+		string closestMatch;
+		TStringArray suggestions = new TStringArray;
 		for ( int i = 0; i < locations.Count(); i++ )
 		{
-			string type = "" + locations[i].Type;
+			string type = locations[i].Type;
 
 			if (  m_CurrentCategory != "ALL" && type != m_CurrentCategory ) 
 				continue;
 
-			string name = "" + locations[i].Name;
+			string name = locations[i].Name;
 			name.ToLower();
 
-			if ( (filter != "" && (!name.Contains( filter ))) ) 
+			if (filter != "" && (!name.Contains( filter )))
 				continue;
+			
+			if ( name == filter )
+			{
+				suggestions.Clear();
+				closestMatch = name;
+			}
+			else if ( name.IndexOf(filter) == 0 )
+			{
+				if (!closestMatch)
+					suggestions.Insert(name);
+			}
 
 			m_LstPositionList.AddItem( locations[i].Name, locations[i], 0 );
 		}
+
+		if (suggestions.Count())
+		{
+			suggestions.Sort();
+			closestMatch = suggestions[0];
+		}
+
+		m_Filter.SetTextPreview(closestMatch);
 	}
 
 	override bool OnItemSelected( Widget w, int x, int y, int row, int column, int oldRow, int oldColumn )
