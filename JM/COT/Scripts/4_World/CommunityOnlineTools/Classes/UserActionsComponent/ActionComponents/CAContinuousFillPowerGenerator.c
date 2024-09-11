d72073518d79c22bc11b367492e8beb4c9944403
lava76 <lava76+github@mail.cbw-clan.de>
2024-09-11 20:22:36 +0200

    More quick actions

---
+++ JM/COT/Scripts/4_World/CommunityOnlineTools/Classes/UserActionsComponent/ActionComponents/CAContinuousFillPowerGenerator.c
@@ -0,0 +1,15 @@
+modded class CAContinuousFillPowerGenerator
+{	
+	override void Setup(ActionData action_data)
+	{
+		super.Setup(action_data);
+
+	#ifdef SERVER
+		PlayerIdentity identity = action_data.m_Player.GetIdentity();
+		if (identity && GetCommunityOnlineToolsBase().IsActive(identity) && GetPermissionsManager().HasPermission("Actions.QuickActions", identity))
+	#else
+		if (GetCommunityOnlineToolsBase().IsActive() && GetPermissionsManager().HasPermission("Actions.QuickActions"))
+	#endif
+			m_AdjustedQuantityFilledPerSecond *= 100;
+	}
+}
