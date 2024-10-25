15cc561a76b5868607320652bba9fa7e3391f721
LieutenantMaster <46745541+LieutenantMaster@users.noreply.github.com>
2024-10-25 17:47:56 +0200

    minor cleanup for my ocd

--- JM/COT/Scripts/4_World/CommunityOnlineTools/Entities/ItemBase/TrapBase/Trap_Bear.c
+++ JM/COT/Scripts/4_World/CommunityOnlineTools/Entities/ItemBase/TrapBase/Trap_Bear.c
@@ -1,14 +1,9 @@
 modded class BearTrap
-{
-	//override void COT_OnDebugSpawn(PlayerBase player)
-	//{
-		//SetActive();
-	//}
-	
+{	
 	override void SetActions()
 	{
 		super.SetActions();
 		
 		AddAction(COT_ActionTriggerBearTrap);
 	}
-}
+};
