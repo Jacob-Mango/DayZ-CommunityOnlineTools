50a7f0d08a3b4793ef1d4ecaea4845074080131b
lava76 <lava76+github@mail.cbw-clan.de>
2025-02-08 18:07:48 +0100

    Call OnDebugSpawn when spawning NPCs via object spawner

--- JM/COT/Scripts/4_World/CommunityOnlineTools/Entities/Player/DayZPlayerImplement.c
+++ JM/COT/Scripts/4_World/CommunityOnlineTools/Entities/Player/DayZPlayerImplement.c
@@ -474,4 +474,16 @@ modded class DayZPlayerImplement
 			return false;
 		return true;
 	}
+
+	//! Prevent calling vanilla EntityAI::OnDebugSpawn
+	//! Logic will use COT to spawn attachments, so it'll work like vanilla EXCEPT not have possibility of crashes due to bad items
+	override void OnDebugSpawn()
+	{
+		GetCommunityOnlineToolsBase().SpawnCompatibleAttachments(this, null, 0);
+	}
+
+	void COT_OnDebugSpawn(PlayerBase player)
+	{
+		OnDebugSpawnEx(DebugSpawnParams.WithPlayer(player));
+	}
 };
