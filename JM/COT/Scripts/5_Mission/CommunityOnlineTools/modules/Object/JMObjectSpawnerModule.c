dae6ed64de7c2ba00ae172ce7e8fefb795e332b8
lava76 <lava76+github@mail.cbw-clan.de>
2025-02-27 13:41:44 +0100

    ifndef -> ifdef

--- JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Object/JMObjectSpawnerModule.c
+++ JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/Object/JMObjectSpawnerModule.c
@@ -691,7 +691,7 @@ class JMObjectSpawnerModule: JMRenderableModuleBase
 		}
 
 		float maxHealth = MiscGameplayFunctions.GetTypeMaxGlobalHealth(entity.GetType());
-	#ifndef COT_WORKAROUND_T188367
+	#ifdef COT_WORKAROUND_T188367
 		if (maxHealth > 0 && (!entity.IsInherited(Container_Base) || entity.GetInventory().GetAttachmentSlotsCount() == 0))
 	#else
 		if (maxHealth > 0)
