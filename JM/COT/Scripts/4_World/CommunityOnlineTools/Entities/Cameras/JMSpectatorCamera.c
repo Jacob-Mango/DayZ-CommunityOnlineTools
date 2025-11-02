de9182358c5ea4014ede1e44850f36f98b71d921
lava76 <lava76+github@mail.cbw-clan.de>
2025-11-02 02:58:12 +0100

    Improved spectator dolly camera
    - Follows spectated player path
    - Avoids clipping into objects

--- JM/COT/Scripts/4_World/CommunityOnlineTools/Entities/Cameras/JMSpectatorCamera.c
+++ JM/COT/Scripts/4_World/CommunityOnlineTools/Entities/Cameras/JMSpectatorCamera.c
@@ -1,10 +1,31 @@
 class JMSpectatorCamera: JMCameraBase
 {
+	static const int DOLLY_CAM_PATH_COUNT = 100;
+
 	vector linearVelocity;
 	vector angularVelocity;
 
 	vector orientation;
 	bool m_COT_IsInFreeLook;
+
+	vector m_JM_CameraPosMS;
+	
+	private float m_COT_SmoothVelDir0[1];
+	private float m_COT_SmoothVelDir1[1];
+	private float m_COT_SmoothVelDir2[1];
+	
+	private float m_COT_SmoothVelPos0[1];
+	private float m_COT_SmoothVelPos1[1];
+	private float m_COT_SmoothVelPos2[1];
+
+	protected vector m_COT_LastPlayerPos;
+
+	vector m_COT_DollyCamPath[100];
+	vector m_COT_DollyCamOri[100];
+	int m_COT_DollyCamPathIdx;
+	float m_COT_DollyCamPathUpdateDT;
+	float m_COT_DollyCamSpeedMult = 0.433333;
+	float m_COT_DollyCamSpeedMultTgt = 0.433333;
 	
 	override void OnTargetSelected( Object target )
 	{
@@ -67,5 +88,356 @@ class JMSpectatorCamera: JMCameraBase
 
 			SetOrientation( orientation );
 		}
+	}
+
+	void OnUpdateEx(DayZPlayerImplement spectatedPlayer, float timeSlice)
+	{
+		vector playerTransform[4];
+		spectatedPlayer.GetTransform(playerTransform);
+		vector playerPos = playerTransform[3];
+
+		vector headTransform[4];
+		spectatedPlayer.GetBoneTransformWS(spectatedPlayer.GetBoneIndexByName( "Head" ), headTransform);
+		vector headPos = headTransform[3];
+
+		float offsetY = headPos[1] - playerPos[1];
+
+		EntityAI hands = spectatedPlayer.GetHumanInventory().GetEntityInHands();
+
+		Weapon_Base weapon;
+		vector weaponTransform[4];
+		//ItemOptics optic;
+		//vector opticTransform[4];
+
+		vector eyePos;
+
+		vector dir;
+		vector pos;
+
+		float fov;
+
+		bool weaponRaised;
+
+		if (Class.CastTo(weapon, hands))
+		{
+			//optic = weapon.GetAttachedOptics();
+			weapon.GetTransform(weaponTransform);
+			eyePos = weapon.GetSelectionPositionLS("eye").Multiply4(weaponTransform);
+			vector rHandPos = spectatedPlayer.GetBonePositionWS(spectatedPlayer.GetBoneIndexByName("RightHand"));
+			if (vector.DistanceSq(headPos, rHandPos) < 0.09)
+				weaponRaised = true;
+			m_JM_IsADS = IsActive() && weaponRaised && vector.DistanceSq(eyePos, headPos) < 0.04;
+		}
+		else
+		{
+			m_JM_IsADS = false;
+		}
+
+		if ( !IsActive() )
+		{
+			if (spectatedPlayer.m_JM_IsHeadInvisible)
+				spectatedPlayer.SetHeadInvisible(false);
+
+			return;
+		}
+
+		bool dollyCam;
+		float speed;
+		switch (m_JM_3rdPerson)
+		{
+			case JMCamera3rdPersonMode.DOLLY:
+				if (!m_COT_IsInFreeLook)
+				{
+					dollyCam = true;
+					speed = 0.9;  //! Slow interpolation, resulting in movement like camera on a dolly
+					break;
+				}
+			default:
+				speed = 0.3;
+				break;
+		}
+
+		if (m_JM_IsADS)
+		{
+			//if (optic)
+			//{
+				//optic.GetCameraPoint(pos, dir);
+				//optic.GetTransform(opticTransform);
+				//pos = pos.Multiply4(opticTransform);
+
+				//if (optic.GetCurrentStepFOV() < GameConstants.DZPLAYER_CAMERA_FOV_IRONSIGHTS)
+				//{
+					//if (!optic.IsInOptics())
+					//{
+						//optic.EnterOptics();
+						//optic.OnOpticEnter();
+					//}
+				//}
+				//else if (optic.IsInOptics())
+				//{
+					//optic.ExitOptics();
+					//optic.OnOpticExit();
+				//}
+			//}
+			//else
+			//{
+				//weapon.GetCameraPoint(weapon.GetCurrentMuzzle(), pos, dir);
+				//pos = pos.Multiply4(weaponTransform);
+			//}
+
+			vector barrel_start = weapon.GetSelectionPositionLS("konec hlavne").Multiply4(weaponTransform);
+			vector barrel_end = weapon.GetSelectionPositionLS("usti hlavne").Multiply4(weaponTransform);
+			dir = vector.Direction(barrel_start, barrel_end).Normalized();
+			pos = eyePos - dir * 0.06;
+
+			if (!m_JM_3rdPerson)
+				fov = GameConstants.DZPLAYER_CAMERA_FOV_IRONSIGHTS;
+			else if (GetUApi().GetInputByID(UAZoomIn).LocalValue())
+				fov = GameConstants.DZPLAYER_CAMERA_FOV_EYEZOOM;
+			else
+				fov = GetDayZGame().GetUserFOV();
+		}
+		else
+		{
+			//if (optic)
+			//{
+				//if (optic.IsInOptics())
+				//{
+					//optic.ExitOptics();
+					//optic.OnOpticExit();
+				//}
+			//}
+
+			vector wl;
+			EWaterLevels waterLevel = DayZPlayerUtils.CheckWaterLevel(spectatedPlayer, wl);
+
+			//float waterDepth = wl[0];
+			//float characterDepth = wl[1];
+
+			//if (waterDepth > 1.5 && characterDepth > 0.0)
+			if (waterLevel == EWaterLevels.LEVEL_SWIM_START)
+			{
+				//! Swimming
+				dir = playerTransform[2];
+				if (m_JM_3rdPerson)
+					pos = headPos + "0 0.5 0";
+				else
+					pos = headPos + "0 0.1 0";
+			}
+			else
+			{
+				dir = headTransform[1];
+				pos = headPos + "0 0.1 0";
+			}
+
+			if (GetUApi().GetInputByID(UAZoomIn).LocalValue())
+				fov = GameConstants.DZPLAYER_CAMERA_FOV_EYEZOOM;
+			else
+				fov = GetDayZGame().GetUserFOV();
+		}
+
+		vector targetPos = pos;
+
+		vector fromOri = GetOrientation();
+		vector targetOri = Math.COT_DirToOri(dir);
+
+		float yawDiff;
+		float pitchDiff;
+
+		float dollyCamSpeedMultMin = 0.333333;  //! Fastest
+		float dollyCamSpeedMultMax = 1.0;  //! Slowest
+
+		bool isUnderRoofBuilding = IsUnderRoofBuilding(spectatedPlayer);
+
+		if (isUnderRoofBuilding)
+		{
+			m_COT_DollyCamSpeedMult = Math.Lerp(m_COT_DollyCamSpeedMult, Math.Min(m_COT_DollyCamSpeedMultTgt, 0.666666), timeSlice);
+		}
+		else
+		{
+			if (GetUApi().GetInputByID(UACameraToolSpeedIncrease).LocalValue())
+				m_COT_DollyCamSpeedMultTgt = Math.Clamp(m_COT_DollyCamSpeedMultTgt - 0.1, dollyCamSpeedMultMin, dollyCamSpeedMultMax);
+			else if (GetUApi().GetInputByID(UACameraToolSpeedDecrease).LocalValue())
+				m_COT_DollyCamSpeedMultTgt = Math.Clamp(m_COT_DollyCamSpeedMultTgt + 0.1, dollyCamSpeedMultMin, dollyCamSpeedMultMax);
+
+			m_COT_DollyCamSpeedMult = Math.Lerp(m_COT_DollyCamSpeedMult, m_COT_DollyCamSpeedMultTgt, timeSlice * 4);
+		}
+
+		vector ori;
+
+		yawDiff = Math.COT_AngleDiff2(fromOri[0], targetOri[0]);
+		pitchDiff = Math.COT_AngleDiff2(fromOri[1], targetOri[1]);
+
+		targetOri[0] = fromOri[0] + yawDiff;
+		targetOri[1] = fromOri[1] + pitchDiff;
+
+		if (m_JM_3rdPerson)
+		{
+			ori[0] = Math.SmoothCD(fromOri[0], targetOri[0], m_COT_SmoothVelDir0, speed, 1000, timeSlice);
+			ori[1] = Math.SmoothCD(fromOri[1], targetOri[1], m_COT_SmoothVelDir1, speed, 1000, timeSlice);
+		}
+		else
+		{
+			ori = vector.Lerp(fromOri, targetOri, timeSlice * CAMERA_FOV_SPEED_MODIFIER);
+		}
+
+		m_COT_DollyCamPathUpdateDT += timeSlice;
+
+		if (m_COT_DollyCamPathUpdateDT > 0.1 * m_COT_DollyCamSpeedMult)
+		{
+			m_COT_DollyCamPathUpdateDT = 0;
+
+			if (m_COT_DollyCamPathIdx < Math.Clamp(9 * m_COT_DollyCamSpeedMult, 1, DOLLY_CAM_PATH_COUNT - 1))
+			{
+				m_COT_DollyCamPath[m_COT_DollyCamPathIdx] = pos;
+				m_COT_DollyCamOri[m_COT_DollyCamPathIdx] = targetOri;
+
+				++m_COT_DollyCamPathIdx;
+			}
+		}
+
+		if (dollyCam)
+		{
+			if (m_COT_DollyCamPathIdx > 0)
+			{
+				pos = m_COT_DollyCamPath[0];
+
+				targetOri = m_COT_DollyCamOri[0];
+			}
+
+			if (m_COT_DollyCamSpeedMult > dollyCamSpeedMultMin)
+			{
+				yawDiff = Math.COT_AngleDiff2(fromOri[0], targetOri[0]);
+				pitchDiff = Math.COT_AngleDiff2(fromOri[1], targetOri[1]);
+
+				targetOri[0] = fromOri[0] + yawDiff;
+				targetOri[1] = fromOri[1] + pitchDiff;
+
+				targetOri[0] = Math.SmoothCD(fromOri[0], targetOri[0], m_COT_SmoothVelDir0, speed, 1000, timeSlice);
+				targetOri[1] = Math.SmoothCD(fromOri[1], targetOri[1], m_COT_SmoothVelDir1, speed, 1000, timeSlice);
+
+				float t = Math.InverseLerp(dollyCamSpeedMultMin, dollyCamSpeedMultMax, m_COT_DollyCamSpeedMult);
+				ori[0] = Math.Lerp(ori[0], targetOri[0], t);
+				ori[1] = Math.Lerp(ori[1], targetOri[1], t);
+			}
+		}
+
+		ori = ori.GetRelAngles();
+		dir = ori.AnglesToVector();
+
+		vector cameraPos = GetPosition();
+		float cameraDistToTargetSq = vector.DistanceSq(cameraPos, targetPos);
+
+		float targetDist;
+		if (m_JM_3rdPerson)
+		{
+			float offsetFactor = 1.0;
+
+			targetDist = 1.33;
+
+			if (dollyCam)
+			{
+				vector vel = (playerPos - m_COT_LastPlayerPos) * (1.0 / timeSlice);  //! GetVelocity(spectatedPlayer) returns 0 on client
+				targetDist *= Math.Max(1.0 - vel.Length() / 6.756, 0.0);
+
+				if (cameraDistToTargetSq > targetDist)
+					offsetFactor = targetDist / cameraDistToTargetSq;
+			}
+			else
+			{
+				pos = pos - dir * targetDist;
+			}
+
+			vector offsetX = dir.Perpend() * 0.33 * offsetFactor;
+
+			if (m_JM_LeftShoulder)
+				pos = pos + offsetX;
+			else
+				pos = pos - offsetX;
+		}
+		else if (!m_JM_IsADS)
+		{
+			targetDist = 0.06;
+			pos = pos + playerTransform[2] * targetDist;
+		}
+
+		m_COT_LastPlayerPos = playerPos;
+
+		SetOrientation(ori);
+
+		if (m_COT_DollyCamPathIdx > 1 && m_COT_DollyCamPathUpdateDT == 0 && cameraDistToTargetSq > 0.25)
+		{
+			for (int i = 0; i < m_COT_DollyCamPathIdx - 1; ++i)
+			{
+				m_COT_DollyCamPath[i] = m_COT_DollyCamPath[i + 1];
+				m_COT_DollyCamOri[i] = m_COT_DollyCamOri[i + 1];
+			}
+
+			m_COT_DollyCamPathIdx -= 1;
+		}
+
+		if (m_JM_CameraPosMS == vector.Zero)
+		{
+			cameraPos = pos;
+			m_JM_CameraPosMS = pos.InvMultiply4(playerTransform);
+		}
+		else
+		{
+			if (dollyCam)
+			{
+				//! Interpolate in world space to give cinematic look
+
+				cameraPos[0] = Math.SmoothCD(cameraPos[0], pos[0], m_COT_SmoothVelPos0, 0.9 * m_COT_DollyCamSpeedMult, 1000, timeSlice);
+				cameraPos[1] = Math.SmoothCD(cameraPos[1], pos[1], m_COT_SmoothVelPos1, 0.8 * m_COT_DollyCamSpeedMult, 1000, timeSlice);
+				cameraPos[2] = Math.SmoothCD(cameraPos[2], pos[2], m_COT_SmoothVelPos2, 0.9 * m_COT_DollyCamSpeedMult, 1000, timeSlice);
+
+				m_JM_CameraPosMS = cameraPos.InvMultiply4(playerTransform);
+			}
+			else
+			{
+				//! Interpolate in model space so camera sticks to character
+				pos = pos.InvMultiply4(playerTransform);
+
+				m_JM_CameraPosMS = vector.Lerp(m_JM_CameraPosMS, pos, timeSlice * CAMERA_FOV_SPEED_MODIFIER);
+
+				//m_JM_CameraPosMS[0] = Math.SmoothCD(m_JM_CameraPosMS[0], pos[0], m_COT_SmoothVelPos0, 0.3, 1000, timeSlice);
+				//m_JM_CameraPosMS[1] = Math.SmoothCD(m_JM_CameraPosMS[1], pos[1], m_COT_SmoothVelPos1, 0.3, 1000, timeSlice);
+				//m_JM_CameraPosMS[2] = Math.SmoothCD(m_JM_CameraPosMS[2], pos[2], m_COT_SmoothVelPos2, 0.3, 1000, timeSlice);
+
+				cameraPos = m_JM_CameraPosMS.Multiply4(playerTransform);
+			}
+		}
+
+		float surfaceY = GetGame().SurfaceRoadY3D(cameraPos[0], cameraPos[1] - offsetY, cameraPos[2], RoadSurfaceDetection.LEGACY) + 0.1;
+		if (surfaceY > cameraPos[1])
+			cameraPos[1] = surfaceY;
+
+		SetPosition( cameraPos );
+
+		float cameraDistanceToHeadSq = vector.DistanceSq(GetPosition(), headPos);
+		if (cameraDistanceToHeadSq < 0.0625 && !spectatedPlayer.m_JM_IsHeadInvisible)
+			spectatedPlayer.SetHeadInvisible(true);
+		else if (m_JM_3rdPerson && cameraDistanceToHeadSq >= 0.0625 && spectatedPlayer.m_JM_IsHeadInvisible)
+			spectatedPlayer.SetHeadInvisible(false);
+
+		SetFOV( Math.Lerp(GetCurrentFOV(), fov, timeSlice * CAMERA_FOV_SPEED_MODIFIER) );
+	}
+
+	bool IsUnderRoofBuilding(DayZPlayerImplement player)
+	{
+		//! if inside vehicle return immediately
+		if (player.IsInVehicle())
+			return false;
+		
+		float hitFraction;
+		vector hitPosition, hitNormal;
+		vector from = player.GetPosition();
+		vector to = from + "0 25 0";
+		Object hitObject;
+		PhxInteractionLayers collisionLayerMask = PhxInteractionLayers.ITEM_LARGE|PhxInteractionLayers.BUILDING|PhxInteractionLayers.VEHICLE;
+		
+		DayZPhysics.RayCastBullet(from, to, collisionLayerMask, null, hitObject, hitPosition, hitNormal, hitFraction);
+
+		return hitObject && hitObject.IsInherited(House);
 	}
 };
--- JM/COT/Scripts/4_World/CommunityOnlineTools/Entities/Player/DayZPlayerImplement.c
+++ JM/COT/Scripts/4_World/CommunityOnlineTools/Entities/Player/DayZPlayerImplement.c
@@ -3,7 +3,6 @@ modded class DayZPlayerImplement
 	private JMSpectatorCamera m_SpectatorCamera;
 	private Head_Default m_PlayerHead;
 	bool m_JM_IsHeadInvisible;
-	vector m_JM_CameraPosMS;
 
 	vector m_COT_HeadBonePositionWS;
 	float m_COT_HeadBoneIdleTime;
@@ -17,29 +16,11 @@ modded class DayZPlayerImplement
 	float m_COT_RFootBoneIdleTime;
 
 	bool m_COT_EnableBonePositionUpdate;
-	
-	private float m_SmoothVelDir0[1];
-	private float m_SmoothVelDir1[1];
-	private float m_SmoothVelDir2[1];
-	
-	private float m_SmoothVelPos0[1];
-	private float m_SmoothVelPos1[1];
-	private float m_SmoothVelPos2[1];
-
-	protected vector m_COT_LastPlayerPos;
 
 	void DayZPlayerImplement()
 	{
 		if ( IsMissionClient() )
 			SetEventMask( EntityEvent.FRAME | EntityEvent.POSTFRAME );
-
-		m_SmoothVelDir0[0] = 0.0;
-		m_SmoothVelDir1[0] = 0.0;
-		m_SmoothVelDir2[0] = 0.0;
-
-		m_SmoothVelPos0[0] = 0.0;
-		m_SmoothVelPos1[0] = 0.0;
-		m_SmoothVelPos2[0] = 0.0;
 	}
 
 	override void CommandHandler( float pDt, int pCurrentCommandID, bool pCurrentCommandFinished )	
@@ -57,7 +38,7 @@ modded class DayZPlayerImplement
 		if (m_SpectatorCamera && !m_SpectatorCamera.m_JM_3rdPerson)
 			SetHeadInvisible( true );
 
-		m_JM_CameraPosMS = vector.Zero;
+		m_SpectatorCamera.m_JM_CameraPosMS = vector.Zero;
 	}
 
 	void OnSpectateEnd()
@@ -73,236 +54,7 @@ modded class DayZPlayerImplement
 		if ( !m_SpectatorCamera )
 			return;
 
-		vector playerTransform[4];
-		GetTransform(playerTransform);
-		vector playerPos = playerTransform[3];
-
-		vector headTransform[4];
-		GetBoneTransformWS(GetBoneIndexByName( "Head" ), headTransform);
-		vector headPos = headTransform[3];
-
-		float offsetY = headPos[1] - playerPos[1];
-
-		EntityAI hands = GetHumanInventory().GetEntityInHands();
-
-		Weapon_Base weapon;
-		vector weaponTransform[4];
-		//ItemOptics optic;
-		//vector opticTransform[4];
-
-		vector eyePos;
-
-		vector dir;
-		vector pos;
-
-		float fov;
-
-		bool weaponRaised;
-
-		if (Class.CastTo(weapon, hands))
-		{
-			//optic = weapon.GetAttachedOptics();
-			weapon.GetTransform(weaponTransform);
-			eyePos = weapon.GetSelectionPositionLS("eye").Multiply4(weaponTransform);
-			vector rHandPos = GetBonePositionWS(GetBoneIndexByName("RightHand"));
-			if (vector.DistanceSq(headPos, rHandPos) < 0.09)
-				weaponRaised = true;
-			m_SpectatorCamera.m_JM_IsADS = m_SpectatorCamera.IsActive() && weaponRaised && vector.DistanceSq(eyePos, headPos) < 0.04;
-		}
-		else
-		{
-			m_SpectatorCamera.m_JM_IsADS = false;
-		}
-
-		if ( !m_SpectatorCamera.IsActive() )
-		{
-			if (m_JM_IsHeadInvisible)
-				SetHeadInvisible(false);
-
-			return;
-		}
-
-		bool dollyCam;
-		float speed;
-		switch (m_SpectatorCamera.m_JM_3rdPerson)
-		{
-			case JMCamera3rdPersonMode.DOLLY:
-				if (!m_SpectatorCamera.m_COT_IsInFreeLook)
-				{
-					dollyCam = true;
-					speed = 0.9;  //! Slow interpolation, resulting in movement like camera on a dolly
-					break;
-				}
-			default:
-				speed = 0.3;
-				break;
-		}
-
-		if (m_SpectatorCamera.m_JM_IsADS)
-		{
-			//if (optic)
-			//{
-				//optic.GetCameraPoint(pos, dir);
-				//optic.GetTransform(opticTransform);
-				//pos = pos.Multiply4(opticTransform);
-
-				//if (optic.GetCurrentStepFOV() < GameConstants.DZPLAYER_CAMERA_FOV_IRONSIGHTS)
-				//{
-					//if (!optic.IsInOptics())
-					//{
-						//optic.EnterOptics();
-						//optic.OnOpticEnter();
-					//}
-				//}
-				//else if (optic.IsInOptics())
-				//{
-					//optic.ExitOptics();
-					//optic.OnOpticExit();
-				//}
-			//}
-			//else
-			//{
-				//weapon.GetCameraPoint(weapon.GetCurrentMuzzle(), pos, dir);
-				//pos = pos.Multiply4(weaponTransform);
-			//}
-
-			vector barrel_start = weapon.GetSelectionPositionLS("konec hlavne").Multiply4(weaponTransform);
-			vector barrel_end = weapon.GetSelectionPositionLS("usti hlavne").Multiply4(weaponTransform);
-			dir = vector.Direction(barrel_start, barrel_end).Normalized();
-			pos = eyePos - dir * 0.06;
-
-			if (!m_SpectatorCamera.m_JM_3rdPerson)
-				fov = GameConstants.DZPLAYER_CAMERA_FOV_IRONSIGHTS;
-			else if (GetUApi().GetInputByID(UAZoomIn).LocalValue())
-				fov = GameConstants.DZPLAYER_CAMERA_FOV_EYEZOOM;
-			else
-				fov = GetDayZGame().GetUserFOV();
-		}
-		else
-		{
-			//if (optic)
-			//{
-				//if (optic.IsInOptics())
-				//{
-					//optic.ExitOptics();
-					//optic.OnOpticExit();
-				//}
-			//}
-
-			vector wl;
-			EWaterLevels waterLevel = DayZPlayerUtils.CheckWaterLevel(this, wl);
-
-			float waterDepth = wl[0];
-			float characterDepth = wl[1];
-
-			if (waterDepth > 1.5 && characterDepth > 0.0)
-			{
-				//! Swimming
-				dir = playerTransform[2];
-				if (m_SpectatorCamera.m_JM_3rdPerson)
-					pos = headPos + "0 0.5 0";
-				else
-					pos = headPos + "0 0.1 0";
-			}
-			else
-			{
-				dir = headTransform[1];
-				pos = headPos + "0 0.1 0";
-			}
-
-			if (GetUApi().GetInputByID(UAZoomIn).LocalValue())
-				fov = GameConstants.DZPLAYER_CAMERA_FOV_EYEZOOM;
-			else
-				fov = GetDayZGame().GetUserFOV();
-		}
-
-		vector fromDir = m_SpectatorCamera.GetDirection();
-		if (m_SpectatorCamera.m_JM_3rdPerson)
-		{
-			dir[0] = Math.SmoothCD(fromDir[0], dir[0], m_SmoothVelDir0, speed, 1000, timeSlice);
-			dir[1] = Math.SmoothCD(fromDir[1], dir[1], m_SmoothVelDir1, speed, 1000, timeSlice);
-			dir[2] = Math.SmoothCD(fromDir[2], dir[2], m_SmoothVelDir2, speed, 1000, timeSlice);
-		}
-		else
-		{
-			dir = vector.Lerp(fromDir, dir, timeSlice * CAMERA_FOV_SPEED_MODIFIER);
-		}
-
-		float dist;
-		if (m_SpectatorCamera.m_JM_3rdPerson)
-		{
-			dist = 1.33;
-			if (dollyCam)
-			{
-				vector vel = (playerPos - m_COT_LastPlayerPos) * (1.0 / timeSlice);  //! GetVelocity(this) returns 0 on client
-				dist *= Math.Max(1.0 - vel.Length() / 6.756, 0.0);
-			}
-			pos = pos - dir * dist;
-			vector offsetX = dir.Perpend() * 0.33;
-			if (m_SpectatorCamera.m_JM_LeftShoulder)
-				pos = pos + offsetX;
-			else
-				pos = pos - offsetX;
-		}
-		else if (!m_SpectatorCamera.m_JM_IsADS)
-		{
-			dist = 0.06;
-			pos = pos + playerTransform[2] * dist;
-		}
-
-		m_COT_LastPlayerPos = playerPos;
-
-		m_SpectatorCamera.SetDirection( dir );
-
-		vector cameraPos;
-
-		if (m_JM_CameraPosMS == vector.Zero)
-		{
-			cameraPos = pos;
-			m_JM_CameraPosMS = pos.InvMultiply4(playerTransform);
-		}
-		else
-		{
-			cameraPos = m_SpectatorCamera.GetPosition();
-
-			if (dollyCam)
-			{
-				//! Interpolate in world space to give cinematic look
-
-				cameraPos[0] = Math.SmoothCD(cameraPos[0], pos[0], m_SmoothVelPos0, 0.4, 1000, timeSlice);
-				cameraPos[1] = Math.SmoothCD(cameraPos[1], pos[1], m_SmoothVelPos1, 0.3, 1000, timeSlice);
-				cameraPos[2] = Math.SmoothCD(cameraPos[2], pos[2], m_SmoothVelPos2, 0.4, 1000, timeSlice);
-
-				m_JM_CameraPosMS = cameraPos.InvMultiply4(playerTransform);
-			}
-			else
-			{
-				//! Interpolate in model space so camera sticks to character
-				pos = pos.InvMultiply4(playerTransform);
-
-				m_JM_CameraPosMS = vector.Lerp(m_JM_CameraPosMS, pos, timeSlice * CAMERA_FOV_SPEED_MODIFIER);
-
-				//m_JM_CameraPosMS[0] = Math.SmoothCD(m_JM_CameraPosMS[0], pos[0], m_SmoothVelPos0, 0.3, 1000, timeSlice);
-				//m_JM_CameraPosMS[1] = Math.SmoothCD(m_JM_CameraPosMS[1], pos[1], m_SmoothVelPos1, 0.3, 1000, timeSlice);
-				//m_JM_CameraPosMS[2] = Math.SmoothCD(m_JM_CameraPosMS[2], pos[2], m_SmoothVelPos2, 0.3, 1000, timeSlice);
-
-				cameraPos = m_JM_CameraPosMS.Multiply4(playerTransform);
-			}
-		}
-
-		float surfaceY = GetGame().SurfaceRoadY3D(cameraPos[0], cameraPos[1] - offsetY, cameraPos[2], RoadSurfaceDetection.LEGACY) + 0.1;
-		if (surfaceY > cameraPos[1])
-			cameraPos[1] = surfaceY;
-
-		m_SpectatorCamera.SetPosition( cameraPos );
-
-		float cameraDistanceToHeadSq = vector.DistanceSq(m_SpectatorCamera.GetPosition(), headPos);
-		if (cameraDistanceToHeadSq < 0.0625 && !m_JM_IsHeadInvisible)
-			SetHeadInvisible(true);
-		else if (m_SpectatorCamera.m_JM_3rdPerson && cameraDistanceToHeadSq >= 0.0625 && m_JM_IsHeadInvisible)
-			SetHeadInvisible(false);
-
-		m_SpectatorCamera.SetFOV( Math.Lerp(m_SpectatorCamera.GetCurrentFOV(), fov, timeSlice * CAMERA_FOV_SPEED_MODIFIER) );
+		m_SpectatorCamera.OnUpdateEx(this, timeSlice);
 	}
 
 #ifndef SERVER
