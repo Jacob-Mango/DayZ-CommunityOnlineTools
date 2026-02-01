17f8ea6f154645800aa8de6637e6621f19d10501
lava76 <lava76+github@mail.cbw-clan.de>
2026-02-01 23:38:24 +0100

    Removed annoying difference in ESP widget position on players vs everything else

--- JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/ESP/JMESPWidgetHandler.c
+++ JM/COT/Scripts/5_Mission/CommunityOnlineTools/modules/ESP/JMESPWidgetHandler.c
@@ -193,7 +193,7 @@ class JMESPWidgetHandler: ScriptedWidgetEventHandler
 		return Math.Asin( a ) / Math.Acos( a );
 	}
 
-	private vector Player_GetPosition()
+	vector Player_GetTopOfHeadPosition()
 	{
 		Human man;
 		if ( !Class.CastTo( man, Info.target ) )
@@ -206,7 +206,7 @@ class JMESPWidgetHandler: ScriptedWidgetEventHandler
 			return man.GetPosition() + "0 1.1 0";
 		}
 
-		vector position = man.GetPosition() + "0 1.85 0";
+		vector position = man.GetPosition() + "0 1.8 0";
 
 		int bone = man.GetBoneIndexByName( "Head" );
 
@@ -222,13 +222,7 @@ class JMESPWidgetHandler: ScriptedWidgetEventHandler
 	{
 		if ( Info.target )
 		{
-			if ( Info.type.IsInherited( JMESPViewTypePlayer ) )
-			{
-				return Player_GetPosition();
-			} else
-			{
-				return Info.target.GetPosition();
-			}
+			return Info.target.GetPosition();
 		}
 
 		return m_LastPosition;
